44ceb97f35de93803845ca258842bc9687052b48
Buganini Q <buganini@gmail.com>
2011-10-13 07:45:51 +0800

    minor tweaks

--- bsdconv.c
+++ bsdconv.c
@@ -2,24 +2,24 @@
 #include <bsdconv.h>
 
 void Init_bsdconv();
-VALUE method_create(VALUE);
-VALUE method_conv(VALUE, VALUE);
+static VALUE m_create(VALUE, VALUE);
+static VALUE m_conv(VALUE, VALUE);
 
-VALUE Bsdconv = Qnil;
+VALUE Bsdconv;
 
 void Init_bsdconv(){
 	Bsdconv = rb_define_class("bsdconv", rb_cObject);
-	rb_define_method(Bsdconv, "initialize", method_create, 1);
-	rb_define_method(Bsdconv, "conv", method_conv, 1);
+	rb_define_method(Bsdconv, "initialize", m_create, 1);
+	rb_define_method(Bsdconv, "conv", m_conv, 1);
 }
 
-VALUE method_create(VALUE conversion){
+static VALUE m_create(VALUE self, VALUE conversion){
 	struct bsdconv_instance *ins;
 	ins=bsdconv_create(RSTRING(conversion)->ptr);
 	return Data_Wrap_Struct(Bsdconv, 0, bsdconv_destroy, ins);
 }
 
-VALUE method_conv(VALUE self, VALUE str){
+static VALUE m_conv(VALUE self, VALUE str){
 	struct bsdconv_instance *ins;
 	Data_Get_Struct(self, struct bsdconv_instance, ins);
 	bsdconv_init(ins);
