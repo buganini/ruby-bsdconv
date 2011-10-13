662e3a0b5bee1dcaf0048f457fe68d6fc614f348
Buganini Q <buganini@gmail.com>
2011-10-13 15:11:39 +0800

    improve libbsdconv detection

--- extconf.rb
+++ extconf.rb
@@ -4,7 +4,7 @@ extension_name = 'bsdconv'
 
 dir_config(extension_name)
 
-have_library('bsdconv', 'bsdconv_create')
+have_library('bsdconv', func='bsdconv_create', header='bsdconv.h')
 have_header('bsdconv.h')
 
 create_makefile(extension_name)
