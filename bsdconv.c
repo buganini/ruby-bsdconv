c26e420a65cb1e0709a00581968e28b12d48d3a0
Buganini Q <buganini@gmail.com>
2011-10-18 13:48:11 +0800

    implement .conv_file()

--- bsdconv.c
+++ bsdconv.c
@@ -1,12 +1,15 @@
 #include <ruby.h>
 #include <bsdconv.h>
 
+#define IBUFLEN 1024
+
 void Init_bsdconv();
 static VALUE m_new(VALUE, VALUE);
 static VALUE m_conv(VALUE, VALUE);
 static VALUE m_init(VALUE);
 static VALUE m_conv_chunk(VALUE, VALUE);
 static VALUE m_conv_chunk_last(VALUE, VALUE);
+static VALUE m_conv_file(VALUE, VALUE, VALUE);
 static VALUE m_info(VALUE);
 static VALUE m_nil(VALUE);
 static VALUE m_error(VALUE);
@@ -18,6 +21,7 @@ void Init_bsdconv(){
 	rb_define_method(Bsdconv, "init", m_init, 0);
 	rb_define_method(Bsdconv, "conv_chunk", m_conv_chunk, 1);
 	rb_define_method(Bsdconv, "conv_chunk_last", m_conv_chunk_last, 1);
+	rb_define_method(Bsdconv, "conv_file", m_conv_file, 2);
 	rb_define_method(Bsdconv, "info", m_info, 0);
 	rb_define_method(Bsdconv, "nil?", m_nil, 0);
 	rb_define_method(Bsdconv, "error", m_error, 0);
@@ -84,6 +88,56 @@ static VALUE m_conv_chunk_last(VALUE self, VALUE str){
 	ret=rb_str_new(ins->output.data, ins->output.len);
 	free(ins->output.data);
 	return ret;
+}
+
+static VALUE m_conv_file(VALUE self, VALUE ifile, VALUE ofile){
+	struct bsdconv_instance *ins;
+	FILE *inf, *otf;
+	char *s1=RSTRING(ifile)->ptr;
+	char *s2=RSTRING(ofile)->ptr;
+	char *in;
+	char *tmp;
+	int fd;
+
+	Data_Get_Struct(self, struct bsdconv_instance, ins);
+	inf=fopen(s1,"r");
+	if(!inf){
+		return Qfalse;
+	}
+	tmp=malloc(strlen(s2)+8);
+	strcpy(tmp, s2);
+	strcat(tmp, ".XXXXXX");
+	if((fd=mkstemp(tmp))==-1){
+		free(tmp);
+		return Qfalse;
+	}
+	otf=fdopen(fd,"w");
+	if(!otf){
+		free(tmp);
+		return Qfalse;
+	}
+
+	bsdconv_init(ins);
+	do{
+		in=malloc(IBUFLEN);
+		ins->input.data=in;
+		ins->input.len=fread(in, 1, IBUFLEN, inf);
+		ins->input.flags|=F_FREE;
+		if(ins->input.len==0){
+			ins->flush=1;
+		}
+		ins->output_mode=BSDCONV_FILE;
+		ins->output.data=otf;
+		bsdconv(ins);
+	}while(ins->flush==0);
+
+	fclose(inf);
+	fclose(otf);
+	unlink(s2);
+	rename(tmp,s2);
+	free(tmp);
+
+	return Qtrue;
 }
 
 static VALUE m_info(VALUE self){
