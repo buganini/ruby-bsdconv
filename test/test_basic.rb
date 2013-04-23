982c1dd14b4359abb63cb14ba6d55cbb8bd577d4
Buganini Q <buganini@gmail.com>
2013-04-23 23:12:40 +0800

    move example4 into test

--- test/test_basic.rb
+++ test/test_basic.rb
@@ -9,5 +9,21 @@ class TestBasic < Test::Unit::TestCase
     c.init
     assert_equal '許功蓋',
       c.conv_chunk("\263\134\245\134\273\134").force_encoding('utf-8')
+
+    sin="utf-8:utf-8,ascii"
+    sout=Bsdconv.insert_phase(sin, "upper", Bsdconv::INTER, 1)
+    assert_equal "UTF-8:UPPER:UTF-8,ASCII", sout
+
+    sin=sout
+    sout=Bsdconv.replace_phase(sin, "full", Bsdconv::INTER, 1)
+    assert_equal "UTF-8:FULL:UTF-8,ASCII", sout
+
+    sin=sout
+    sout=Bsdconv.replace_codec(sin, "big5", 2, 1)
+    assert_equal "UTF-8:FULL:UTF-8,BIG5", sout
+
+    sin=sout
+    sout=Bsdconv.insert_codec(sin, "ascii", 0, 1)
+    assert_equal "UTF-8,ASCII:FULL:UTF-8,BIG5", sout
   end
 end
