9f5803bcd557c2cc214c03f7a1404c6f478d0f7b
Buganini Q <buganini@gmail.com>
2013-11-11 08:08:48 +0800

    add example_insert_replace.rb

--- example/example_codecs.rb
+++ example/example_codecs.rb
@@ -2,15 +2,6 @@
 
 require 'bsdconv'
 
-c=Bsdconv.new('utf-8:utf-8')
-
-if c.nil?
-	abort(Bsdconv.error())
-end
-
-c.insert_phase('full',Bsdconv::INTER, 1)
-puts c.conv('test')
-
 p Bsdconv.codec_check(Bsdconv::FROM, "_utf-8")
 p Bsdconv.codec_check(Bsdconv::INTER, "_utf-8")
 puts 'From:'
